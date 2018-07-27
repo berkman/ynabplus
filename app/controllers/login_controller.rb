0334e818e04e152f1470122b98b89c182b8989b3
Mike Berkman <mberkman@covermymeds.com>
2018-07-27 11:16:59 -0400

    oauth

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,7 +2,14 @@ require 'omniauth-oauth'
 
 class LoginController < ApplicationController
   def main
-    @code = params[:code]
+    @ynab_code = params[:code]
+    @ynab_id = ENV['YNAB_ID']
+    @ynab_secret = ENV['YNAB_SECRET']
+    @ynab_redirect = ENV['YNAB_REDIRECT']
+
+    @ynab_token = params[:access_token]
+
+    @auth = "https://app.youneedabudget.com/oauth/token?client_id=#{@ynab_id}&client_secret=#{@ynab_secret}&redirect_uri=#{@ynab_redirect}&grant_type=authorization_code&code=#{@ynab_code}"
   end
 
   def callback
