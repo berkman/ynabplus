1f74ec84b1249586f9d2af442e622d132ed70ac5
Mike Berkman <mberkman@covermymeds.com>
2018-08-01 12:36:17 -0400

    assistant controller

--- app/controllers/assistant_controller.rb
+++ app/controllers/assistant_controller.rb
@@ -3,13 +3,46 @@ skip_before_action :verify_authenticity_token
 http_basic_authenticate_with name: ENV['ASSISTANT_USERNAME'], password: ENV['ASSISTANT_PASSWORD']
 
   def main
-    assistant = Hash.new
-    assistant[:user] = params[:user] || nil
-    assistant[:category] = params[:category] || nil
+    assistant_request = Hash.new
+    assistant_request[:responseId] = params[:responseId] || nil
+    assistant_request[:queryResult] = params[:queryResult] || nil
+    assistant_request[:originalDetectIntentRequest] = params[:originalDetectIntentRequest] || nil
+    assistant_request[:session] = params[:session] || nil
+
+    assistant_response = Hash.new
+    assistant_response[:responseId] = assistant_request[:responseId]
+    assistant_response[:queryResult] ||= {}
+    assistant_response[:queryResult][:queryText] = assistant_request[:queryResult][:queryText]
+    assistant_response[:queryResult][:action] = assistant_request[:queryResult][:action]
+    assistant_response[:queryResult][:parameters] = assistant_request[:queryResult][:parameters]
+    assistant_response[:queryResult][:allRequiredParamsPresent] = assistant_request[:queryResult][:allRequiredParamsPresent]
+
+    assistant_response[:queryResult][:fulfillmentMessages] ||= {}
+    assistant_response[:queryResult][:fulfillmentMessages][:text] ||= {}
+    assistant_response[:queryResult][:fulfillmentMessages][:text][:text] = "Your category is category!  You have undefined remaining."
+
+    assistant_response[:queryResult][:fulfillmentMessages][:card] ||= {}
+    assistant_response[:queryResult][:fulfillmentMessages][:card][:title] = "Card Title"
+    assistant_response[:queryResult][:fulfillmentMessages][:card][:subtitle] = "This is the body text of a card.  You can even use line\n  breaks and emoji! 💁"
+    assistant_response[:queryResult][:fulfillmentMessages][:card][:imageUri] = "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png"
+    assistant_response[:queryResult][:fulfillmentMessages][:card][:buttons] ||= {}
+    assistant_response[:queryResult][:fulfillmentMessages][:card][:buttons][:text] = "This is a button"
+    assistant_response[:queryResult][:fulfillmentMessages][:card][:buttons][:postback] = "https://assistant.google.com/"
+
+    assistant_response[:queryResult][:fulfillmentMessages][:quickReplies] ||= {}
+    assistant_response[:queryResult][:fulfillmentMessages][:quickReplies][:quickReplies] = "derp", "Suggestion"
+
+    assistant_response[:queryResult][:outputContexts] ||= {}
+    assistant_response[:queryResult][:intent] ||= {}
+    assistant_response[:queryResult][:intentDetectionConfidence] = assistant_request[:queryResult][:intentDetectionConfidence]
+    assistant_response[:queryResult][:diagnosticInfo] ||= {}
+    assistant_response[:queryResult][:languageCode] = assistant_request[:queryResult][:languageCode]
+    assistant_response[:webhookStatus] ||= {}
+    assistant_response[:webhookStatus][:message] = "Webhook execution successful"
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render :json => assistant}
+      format.json { render :json => assistant_response}
     end
   end
 end
