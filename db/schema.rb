f45266293e09132d35d46dff03fd68283f22d526
Mike Berkman <mberkman@covermymeds.com>
2018-08-02 09:41:23 -0400

    new model

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_27_193214) do
+ActiveRecord::Schema.define(version: 2018_08_02_133918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "provider"
     t.string "uid"
     t.string "name"
-    t.string "access_token"
-    t.string "refresh_token"
-    t.string "token_expiration"
-    t.string "budget_id"
+    t.string "oauth_token"
+    t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
