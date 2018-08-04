150884f8e2b324f2316dfaba32c639dde323af69
Mike Berkman <mberkman@covermymeds.com>
2018-08-04 16:22:35 -0400

    removed login

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
 
   post 'assistant', to: 'assistant#main'
 
-  get 'months', to: 'months#main'
   get 'accounts', to: 'accounts#main'
-  get 'categories', to: 'categories#main'
   get 'budgets', to: 'budgets#main'
-  get 'login', to: 'login#main'
+  get 'categories', to: 'categories#main'
+  get 'months', to: 'months#main'
   get 'payees', to: 'payees#main'
 
   get 'auth/:provider/callback', to: 'sessions#create'
--- test/controllers/login_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class LoginControllerTest < ActionDispatch::IntegrationTest
-  test "should get main" do
-    get login_main_url
-    assert_response :success
-  end
-
-end
