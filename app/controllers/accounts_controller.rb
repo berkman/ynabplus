05f91912c042437e9e7a1c94938bb6d10ac90077
Mike Berkman <mberkman@covermymeds.com>
2018-11-27 21:11:54 -0500

    tweak controllers

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,12 +2,18 @@ require 'ynab'
 
 class AccountsController < ApplicationController
   def index
-    @budget = current_user.budgets.first
+    #if !session[:accounts]
+      budget = session[:budgets].first
+      logger.debug budget.to_yaml
 
-    ynab_api = YNAB::API.new(current_user.oauth_token)
+      ynab_api = YNAB::API.new(current_user.oauth_token)
 
-    accounts_response = ynab_api.accounts.get_accounts(@budget.budget_id)
-    @accounts = accounts_response.data.accounts
+      accounts_response = ynab_api.accounts.get_accounts(budget['id'])
+      @accounts = accounts_response.data.accounts
+
+      #session[:accounts] = @accounts
+    #else
+    #end
   end
 
   def show
--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -2,15 +2,22 @@ require 'ynab'
 
 class BudgetsController < ApplicationController
   def index
-    ynab_api = YNAB::API.new(current_user.oauth_token)
+    if !session[:budgets]
+      ynab_api = YNAB::API.new(current_user.oauth_token)
 
-    budgets_response = ynab_api.budgets.get_budgets
-    @budgets = budgets_response.data.budgets
+      budgets_response = ynab_api.budgets.get_budgets
+      @budgets = budgets_response.data.budgets
 
-    session[:budgets] = @budgets
+      session[:budgets] = @budgets
 
-    @budgets.each do |budget|
-      Budget.from_ynab(budget.id, budget.name, current_user.id)
+=begin
+      # create an object but dont persist to the database
+            @budgets.each do |budget|
+              Budget.from_ynab(budget.id, budget.name, current_user.id)
+            end
+=end
+    #else
+      #@budgets = session[:budgets]
     end
   end
 
